255c474d4d2ee74942ca4925e3ffe238eb7d519e
Cameron <kramin42@gmail.com>
2014-03-10 14:00:31 +1300

    added node.js stuff

---
+++ web.js
@@ -0,0 +1,36 @@
+// web.js
+//var url = require("url");
+var fs = require("fs");
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+var oneDay = 86400000;
+
+app.use(logfmt.requestLogger());
+
+//app.use(express.compress());
+
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+app.get('/fireworktext', function(req, res) {
+  //var pathname = url.parse(request.url).pathname;
+  //var pathname = request.params[0];
+  //console.log("Request for " + pathname + " received.");
+  //response.writeHead(200, {"Content-Type": "text/html"});
+  //response.write(fs.readFileSync("./fireworks_text.htm").toString().replace("<INSERT_HERE>","\"Hello\","+"\""+pathname.split("/")[1]+"\""));
+  //response.end();
+  var text = '"'+req.query.text.split(' ').join('","')+'"';
+  console.log(text);
+  res.send(fs.readFileSync("./fireworks_text_template.htm").toString().replace("<INSERT_HERE>",text));
+});
+
+//app.get('/processing-1.4.1.min.js', function(req, res) {
+//	res.set('Content-Type', 'text/plain');
+//  	res.send(fs.readFileSync("./processing-1.4.1.min.js").toString());
+//});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
